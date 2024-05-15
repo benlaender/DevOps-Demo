163efdbf31a166aea46ba86d8a485e2d901b0bff
benlaender <benito@bodtlander.com>
2024-05-15 20:34:13 +0200

    Initial Commit As a differentiation from the other weeks. Sadly the idea only appeared in the last week

---
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/Elevator.java
@@ -0,0 +1,29 @@
+package ch.zhaw.iwi.devops.demo;
+
+public class Elevator 
+{
+    public String call (int start, int ruf)
+    {
+        if(start > 5 || start < -2)
+        {
+            start = 0;
+        }
+
+        if(ruf > 5 || ruf < -2)
+        {
+            return "Wrong building";
+        }
+        else if(start == ruf)
+        {
+            return "Door opens";
+        }
+        else if (start < ruf)
+        {
+            return "Upwards";
+        }
+        else
+        {
+            return "Downwards";
+        }
+    }
+}
---
+++ backend/src/test/java/ch/zhaw/iwi/devops/CallElevator/CallElevator_Test.java
@@ -0,0 +1,65 @@
+package ch.zhaw.iwi.devops.CallElevator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import ch.zhaw.iwi.devops.demo.Elevator;
+
+
+class CallElevator_Test
+{
+    @Test
+    void Test1()
+    {
+        Elevator Button = new Elevator();
+        Assertions.assertEquals("Door opens", Button.call(0, 0));
+    }
+
+    @Test
+    void Test2()
+    {
+        Elevator Button = new Elevator();
+        Assertions.assertEquals("Upwards", Button.call(1, 2));
+    }
+
+    @Test
+    void Test3()
+    {
+        Elevator Button = new Elevator();
+        Assertions.assertEquals("Downwards", Button.call(3, -1));
+    }
+
+    @Test
+    void Test4()
+    {
+        Elevator Button = new Elevator();
+        Assertions.assertEquals("Downwards", Button.call(500, -1));
+    }
+
+    @Test
+    void Test5()
+    {
+        Elevator Button = new Elevator();
+        Assertions.assertEquals("Downwards", Button.call(-100, -1));
+    }
+
+    @Test
+    void Test6()
+    {
+        Elevator Button = new Elevator();
+        Assertions.assertNotEquals("Downwards", Button.call(300,1));
+    }
+
+    @Test
+    void Test7()
+    {
+        Elevator Button = new Elevator();
+        Assertions.assertEquals("Wrong building", Button.call(-2, 500));
+    }
+
+    @Test
+    void Test8()
+    {
+        Elevator Button = new Elevator();
+        Assertions.assertEquals("Upwards", Button.call(1, 2));
+    }
+}
