f41cdc31e61b35ed3d6cffcb964d5e2073f3cb59
benlaender <benito@bodtlander.com>
2024-05-24 13:50:40 +0200

    Trigger Docker Push

--- backend/src/main/java/ch/zhaw/iwi/devops/demo/ToDoController.java
+++ backend/src/main/java/ch/zhaw/iwi/devops/demo/ToDoController.java
@@ -34,6 +34,7 @@ public class ToDoController {
         this.todos.put(4,new ToDo(4, "Deployment", "Jede Woche!"));
         this.todos.put(5,new ToDo(5, "Organigramm", "Löschen"));
         this.todos.put(6,new ToDo(6, "Java bodtlben", "Version 21 zum Laufen bringen!"));
+        this.todos.put(7,new ToDo(7, "Lernjournal", "Das Lernjournal für DevOps fertigstellen!"));
         System.out.println("Init Data");
     }
 
